f50626eff82cddf18cf663863d8ba2b6cff58109
Martin Camer <martincamer011@gmail.com>
2023-07-10 20:30:38 -0300

    amarena-app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: false,
+	pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
 };
 
 module.exports = nextConfig;
