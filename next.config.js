70ad4a624effd6cba135aa6acd26b8e347e70238
Martin Camer <martincamer011@gmail.com>
2023-07-10 23:16:35 -0300

    new

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: false,
+	eventName: 'NEXT_VERSION',
+	payload: {
+		version: '9.0.5-canary.2',
+		isDevelopment: false,
+	},
 };
 
 module.exports = nextConfig;
